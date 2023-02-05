f4918e0b7328ad8b8d125b7fb6894cda1bc4d34d
maithevien9 <vienthe.mai@team.enouvo.com>
2023-02-05 21:43:39 +0700

    Fix rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    resolve(),
-    babel({
-      exclude: '**/node_modules/**',
-    }),
-    commonjs({ include: ['./index.js', 'node_modules/**'] }),
-  ],
+  plugins: [react()],
 });
